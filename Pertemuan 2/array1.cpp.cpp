945470ad1dc3fa5daafcfb68c94f83156507e0ca
Carissaokt <carissa.oktavia12@gmail.com>
2024-03-27 09:34:58 +0700

    pertemuan 7

---
+++ Pertemuan 2/array1.cpp.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	double arr1[9];
+	arr1[0] = 1.2;
+	arr1[1] = 2.6;
+	arr1[2] = 6.7;
+	arr1[3] = 9.4;
+	arr1[4] = 11.5;
+	arr1[5] = 13.9;
+	arr1[6] = 54.7;
+	arr1[7] = 74.3;
+	arr1[8] = 23.6;
+	
+	cout << arr1[0] <<endl;
+	cout << arr1[1] <<endl;
+	cout << arr1[2] <<endl;
+	cout << arr1[3] <<endl;
+	cout << arr1[4] <<endl;
+	cout << arr1[5] <<endl;
+	cout << arr1[6] <<endl;
+	cout << arr1[7] <<endl;
+	cout << arr1[8] <<endl;
+	
+	int arr2[12];
+	arr2[0] = 44; 
+	arr2[1] = 32;
+	arr2[2] = 14;
+	arr2[3] = 87;
+	arr2[4] = 42;
+	arr2[5] = 92;
+	arr2[6] = 73;
+	arr2[7] = 68;
+	arr2[8] = 29;
+	arr2[9] = 22;
+	arr2[10] = 48;
+	arr2[11] = 86;
+	
+	cout << arr2[0] << endl;
+	cout << arr2[1] << endl;
+	cout << arr2[2] << endl;
+	cout << arr2[3] << endl;
+	cout << arr2[4] << endl;
+	cout << arr2[5] << endl;
+	cout << arr2[6] << endl;
+	cout << arr2[7] << endl;
+	cout << arr2[8] << endl;
+	cout << arr2[9] << endl;
+	cout << arr2[10] << endl;
+	cout << arr2[11] << endl;
+	
+}
